0e8cc00e9df84133144cd4b3c9ee97ae7d1f97d5
KillAllChickens <admin@killallchickens.org>
2026-04-12 21:19:42 -0500

    ADD: Go example

---
+++ go_code/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"resty.dev/v3"
+)
+
+func main() {
+	client := resty.New()
+
+	type hydro struct {
+		FuelType string `json:"fuelType"`
+		BmUnit   string `json:"bmUnit"`
+		Name     string `json:"name"`
+	}
+
+	var units []hydro
+
+	_, err := client.R().
+		SetResult(&units).
+		Get("https://data.elexon.co.uk/bmrs/api/v1/reference/bmunits/all")
+	if err != nil {
+		panic(err)
+	}
+
+	hydroTypes := map[string]bool{"NPSHYD": true, "PS": true}
+
+	var filtered []hydro
+	for _, u := range units {
+		if hydroTypes[u.FuelType] {
+			filtered = append(filtered, u)
+		}
+	}
+
+	for _, u := range filtered {
+		fmt.Printf("%-25s  %-8s  %s\n", u.BmUnit, u.FuelType, u.Name)
+	}
+
+	fmt.Printf("\ntotal hydro units found: %d\n", len(filtered))
+}
